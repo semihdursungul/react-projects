a3345c903c602568eecaeea4d540ac124506e02c
Semih Dursungül <114025283+semihdursungul@users.noreply.github.com>
2023-05-22 23:27:20 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To Do List by semihdursungul</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
